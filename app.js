401b5f6e696ef2e8f2c87d9158716ea1729925fb
ary sitepu kantor <aririzkita76@gmail.com>
2024-02-15 16:45:39 +0700

    first upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// for(let i = 1; i <= 10; i++){
+//     console.log('saya ada di dalam loop');
+//     console.log(i);
+// }
+
+// for(let i = 1; i <= 20; i+=2){
+  
+//     console.log(i);
+// }
+// for(let i = 20; i >= 0; i-=2){
+//     console.log(i);
+// }
+// for(let i = 10; i <= 100000; i*=10){
+//     console.log(i);
+// }
+const animals = ['cat', 'deer', 'hog', 'koala', 'zebra'];
+for(let i = 0; i < animals.length; i++){
+ console.log(i + 1, animals[i]);
+}
